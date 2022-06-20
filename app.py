18611286ba5173c69289c3f3b025b060f583fde8
AAspan <93344588+AAspan@users.noreply.github.com>
2022-06-20 18:52:52 -0400

    latest

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
+import html
+
 from flask import Flask, render_template
 app = Flask(__name__)
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/jobs')
+def jobs():
+    return render_template('jobs.html')
+
+@app.route('/event')
+def event():
+    return render_template('event.html')
+
+@app.route('/alerts')
+def alerts():
+    return render_template('alerts.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
