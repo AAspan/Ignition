0a9abe727a479070565d4968c146fb0714990de0
Ravier <konanravier@gmail.com>
2022-07-01 00:56:28 -0400

    Designing the account dashboard page

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 import html
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+
 app = Flask(__name__)
+
+#Database configuration 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'ignition'
+
+mysql = MySQL(app)
+
 
 @app.route('/')
 def home():
@@ -27,7 +38,43 @@ def alerts():
 #Admin
 @app.route('/admin')
 def admin():
+
+    #if request.method == 'GET':
+    #    return "Login via the login Form"
+
+    #name = request.form['name']
+    #age = request.form['age']
+
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM job')
+    rv = cursor.fetchall()
+
+    #return str(rv)
+
     return render_template('admin/index.html')
+
+
+
+#post a job
+@app.route('/admin/postjob')
+def formpostjob():
+
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM job')
+    rv = cursor.fetchall()
+
+    return render_template('admin/post_job.html')
+
+#Dashboard
+@app.route('/admin/dashboard')
+def dashboard():
+    title = "Dashboard"
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM job')
+    rv = cursor.fetchall()
+
+    return render_template('admin/dashboard.html', title = title)
+
 
 
 if __name__ == '__main__':
