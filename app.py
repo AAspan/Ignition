d11b7d9938ef96fd8ebd9a9be0d61c315100b700
Ravier <konanravier@gmail.com>
2022-07-24 17:06:34 -0400

    Added page list application for candidate

--- app.py
+++ app.py
@@ -257,13 +257,17 @@ def addjob():
     return listpostjob()
 
 
-#Show applications list 
+#Show applications list of the Candidate
 @app.route('/admin/my-applications')
 def myapplications():
-    #cursor = mysql.connection.cursor()
-    #cursor.execute('SELECT * FROM application')
-    #rv = cursor.fetchall()
-    return render_template('admin/applications.html')
+    candidate_id=1 # We should get the candidate id from the session
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM application WHERE candidate_id = ' + str(candidate_id) )
+    #cursor.execute(sql_req, data)
+    result = cursor.fetchall()
+    print(result)
+
+    return render_template('admin/applications.html', applications=result)
 
 #Show candidate applications by a recruiter 
 @app.route('/admin/applications/<int:job_id>')
@@ -277,7 +281,6 @@ def applications(job_id):
     for item in result:
         print(item)
 
-    #return "ok"
     return render_template('admin/applications.html', applications=result)
 
 #Show applications list 
